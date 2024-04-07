9db3ac16db1308efd6825f840a7841337eb9f5d3
aryoppa <aryop@upi.edu>
2024-04-08 06:01:25 +0700

    adding labels to json

--- model.py
+++ model.py
@@ -1,63 +1,59 @@
 import os
+import logging
 from utils import load_data, search_notebook
 from openai import OpenAI
 from dotenv import load_dotenv
 
 # Set OpenAI API key
 load_dotenv(override=True)
+OPENAI_MODEL = os.environ.get("OPENAI_MODEL", "gpt-3.5-turbo-0125")
 client = OpenAI(
-    # This is the default and can be omitted
     api_key=os.environ.get("OPENAI_API_KEY"),
 )
+
+def get_openai_response(prompt, model="gpt-3.5-turbo-0125", temperature=0.5, max_tokens=100, stream=True):
+    """Sends a prompt to the OpenAI API and returns the response."""
+    try:
+        response = ""
+        for chunk in client.chat.completions.create(
+                model=model, messages=prompt, stream=stream, temperature=temperature, max_tokens=max_tokens
+        ):
+            text = chunk.choices[0].delta.content
+            if text is not None:
+                response += text
+        return response.strip()
+    except Exception as e:
+        logging.error(f"An error occurred while calling OpenAI API: {e}")
+        return "Maaf, saya tidak bisa menghasilkan respons saat ini. Bagaimana saya bisa membantu Anda?"
 
 # Define function to classify user input as a greeting or question
 def classify_input(user_input: str) -> str:
     """Classifies user input as a greeting or question."""
-    try:
-        if "external data" in user_input.lower():
-            return "Maaf, tetapi Anda tidak memiliki akses ke data eksternal."
-        else:
-            prompt = [
-                 {"role": "system", "content": f"classify user input {user_input} as one of these label 'question' or 'greeting', or 'absurd_question'"},
-            ]
-            response = ""
-            for chunk in client.chat.completions.create(
-                    model="gpt-3.5-turbo-0125", messages=prompt, stream=True, temperature=0.8, max_tokens=15
-            ):
-                text = chunk.choices[0].delta.content
-                if text is not None:
-                    response += text
-            return response.strip()
-    except Exception as e:
-        return "Maaf, saya tidak bisa menghasilkan respons saat ini. Bagaimana saya bisa membantu Anda?"
+    if "external data" in user_input.lower():
+        return "Maaf, tetapi Anda tidak memiliki akses ke data eksternal."
+    else:
+        prompt = [
+             {"role": "system", "content": f"classify user input {user_input} as one of these label 'question' or 'greeting', or 'absurd_question'"},
+        ]
+        return get_openai_response(prompt, model=OPENAI_MODEL, temperature=0.8, max_tokens=15, stream=True)
     
-# Define function to generate response to user greeting
-def answer_greeting(greeting: str) -> str:
-
-    #AI persona name
+# Define function to generate response to user greeting and return a hardcoded label
+def answer_greeting(greeting: str) -> tuple:
+    # AI persona name
     persona_name = "Naura"
     
     # Load data for each question
     df = load_data()
     
-    try:
-        if "external data" in greeting.lower():
-            return "Maaf, tetapi Anda tidak memiliki akses ke data eksternal."
-        else:
-            prompt = [
-                {"role": "system", "content": f"Sapaan pengguna: {greeting}\nApa yang bisa saya bantu hari ini"}
-            ]
-            response = ""
-            for chunk in client.chat.completions.create(
-                    model="gpt-3.5-turbo-0125", messages=prompt, stream=True, temperature=0.5, max_tokens=100
-            ):
-                text = chunk.choices[0].delta.content
-                if text is not None:
-                    response += text
-            return response.strip()
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return "Maaf, saya tidak bisa menghasilkan respons saat ini. Bagaimana saya bisa membantu Anda?"
+    if "external data" in greeting.lower():
+        return ("Maaf, tetapi Anda tidak memiliki akses ke data eksternal.", "access_denied")
+    else:
+        prompt = [
+            {"role": "system", "content": f"Sapaan pengguna: {greeting}\nApa yang bisa saya bantu hari ini"}
+        ]
+        response = get_openai_response(prompt, model=OPENAI_MODEL, temperature=0.5, max_tokens=100, stream=True)
+        label = "greeting_response"
+        return {"message":response, "label":label}
     
 # Define function to process the question and generate response from data
 def process_question(question: str,) -> str:
@@ -74,17 +70,14 @@ def process_question(question: str,) -> str:
                     prompt = [
                         {"role": "system", "content": f"Summarize: {csv_extract}\nBerdasarkan pertanyan berikut: {question}\nMohon jangan mengada-ada atau menambahkan informasi lebih lanjut.\nHanya ambil informasi yang relevan."},
                     ]
-                    response = ""
-                    for chunk in client.chat.completions.create(
-                            model="gpt-3.5-turbo-0125", messages=prompt, stream=True, temperature=0.1, max_tokens=1000
-                    ):
-                        text = chunk.choices[0].delta.content
-                        if text is not None:
-                            response += text
+                    response = get_openai_response(prompt, model=OPENAI_MODEL, temperature=0.1, max_tokens=1000, stream=True)
                     summaries.append(response.strip())
-            return f"\n".join(summaries)
+            responses =f"\n".join(summaries)
+            label = "question_response"
+            return {"message":responses, "label":label}
         else:
             return "Maaf, saya tidak bisa menemukan informasi yang sesuai dengan pertanyaan Anda."
     except TypeError as e:
+        logging.error(f"An error occurred: {e}")
         return "Data diluar konteks yang ada, mohon masukan pertanyaan lainnya"
 
