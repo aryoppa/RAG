ec3af0306fdcdc5d6c908943bbd6b72d76779a12
muhammadrisma <muhammadrisma2003@gmail.com>
2024-05-07 14:49:24 +0700

    Fix Prompt Engineering

--- tracking/trackings.py
+++ tracking/trackings.py
@@ -12,8 +12,8 @@ client = OpenAI(
 MODEL = "gpt-3.5-turbo"
 
 data = f"""
-
-Data Aju 00003002604120231229000013 dengan status berikut:
+1.
+Status Aju 00003002604120231229000013 dengan status berikut:
 
 No Daftar: 600045
 Daftar: 23-01-2024
@@ -41,6 +41,36 @@ Perekaman Perbaikan Portal
 
 2024-01-17 12:36:59.969
 Validasi Perbaikan
+----------------------------------
+2.
+Statu aju 00004002503220231215000018 dengan status berikut:
+
+No Daftar: 700098
+Daftar: 15-12-2023
+
+2023-12-15 09:25:10.753
+Perekaman Dokumen
+
+2023-12-20 16:30:22.441
+Payment Verification Pending
+
+2023-12-21 10:48:45.109
+Pemeriksaan Dokumen
+
+2023-12-21 11:30:00.052
+Siap Jalur
+
+2023-12-21 12:00:15.329
+Penjaluran
+
+2023-12-21 12:01:05.444
+Gate In TPS
+
+2024-01-02 14:20:30.187
+Perekaman Perbaikan Portal
+
+2024-01-02 14:30:59.871
+Validasi Perbaikan.
 
 """
 def process_tracking(tracking: str) -> str:
@@ -48,12 +78,13 @@ def process_tracking(tracking: str) -> str:
         system_message =  f"""
         You will be provided with user queries. \n
         Answer using Bahasa Indonesia. Don't make up the answer. If the answer cannot be found, write 'Maaf saya tidak memiliki data terkait di dalam databse saya' \n
+        If the Aju status number in the user input is less or more than 26 digits, then answer "Nomor Status Aju yang Anda masukkan tidak valid (harus terdiri dari 26 digit). Mohon masukkan Nomor Status Aju yang benar."
         """
         prompt = [  
             {'role':'system', 
             'content': system_message},    
             {'role':'user', 
-            'content': f"Answer user tracking questions:{tracking} based on the information provided in {data}, Give Detail information about the tracking status."},  
+            'content': f"Answer user tracking questions:{tracking} based on the information provided in {data}, Give Detail information about the tracking status."}  
         ] 
         response = client.chat.completions.create(
             model=MODEL, messages=prompt, temperature=0.2, max_tokens=1000
